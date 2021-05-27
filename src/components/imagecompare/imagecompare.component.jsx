f9b5011908855ba38a969ad4fa562a9880f1a9d0
YaseenIqbal309 <70229873+YaseenIqbal309@users.noreply.github.com>
2021-05-27 12:31:25 -0700

    Comparison between pre and post

---
+++ src/components/imagecompare/imagecompare.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { ImgComparisonSlider } from '@img-comparison-slider/react';
+
+import  comp1 from '../../assets/comp1.jpg'
+import comp2 from '../../assets/comp2.jpg'
+import arrows from '../../assets/arrows.png'
+import './imagecompare.styles.css'
+
+const  ImgComare = () => {
+            return (
+            <ImgComparisonSlider class="slider-with-custom-handle">
+            <img slot="before" width="100%" src={comp1}></img>
+            <img slot="after" width="100%" src={comp2}></img>
+            <img slot="handle" class="custom-handle" src={arrows}></img>
+            </ImgComparisonSlider>
+            )
+}
+export default ImgComare;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -21,7 +21,7 @@ const HomePage = () => (
                         </p>
                      </div>
             </div>
-            
+            <ImgComare></ImgComare>
          </Container>
          
       
