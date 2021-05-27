193942e57d37deaf7513550a55dd3a5fc597c200
YaseenIqbal309 <70229873+YaseenIqbal309@users.noreply.github.com>
2021-05-27 12:29:15 -0700

    responsive container

---
+++ src/components/container/container.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './container.styles.css'
+
+const Container = ({children}) => (
+    <div className="container">
+        {children}
+    </div>
+);
+export default Container;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import Video from '../../components/video/video.component'
-
+import Container from '../../components/container/container.component'
+import ImgComare from  '../../components/imagecompare/imagecompare.component'
 import './homepage.styles.css'
+import Title from '../../components/title/title.component'
+
 
 const HomePage = () => (
     <div className="home-content">
         <Video  />
-       
+        <Container>
+            <div className="row wid-parent overflow-wrap">
+                    <div className="col-md-6 col-sm-12">
+                          <Title></Title>
+                    </div>
+                    
+                     <div className="col-md-6 col-sm-12">
+                        <p className="title_description">
+                             We have combined the best-of-breed technology platforms with an eye towards relieving you of administrative burden. Matterport 3D scans augmented by TruePlan are used to generate consistent Xactimate sheets that are delivered in accord with Actionable Insights compliance rule sets. Each mpartial is produced with full transparency, unprecedented forensic photography, and infallible geospatial data that collectively result in rapid approvals.
+                        </p>
+                     </div>
+            </div>
+            
+         </Container>
          
       
     </div>
