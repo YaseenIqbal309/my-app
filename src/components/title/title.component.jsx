fa9fd8abfd58b9fea586c827101a91861f10ba53
YaseenIqbal309 <70229873+YaseenIqbal309@users.noreply.github.com>
2021-05-27 12:30:24 -0700

    Growth Section

---
+++ src/components/title/title.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Container from '../container/container.component'
+import './title.styles.css'
+
+const Title = () =>(
+    
+                            <h1 className="section-title">
+                    
+                                 GROUND-TRUTH DATA EVERYONE TRUSTS
+
+                             </h1>
+          
+    
+);
+export default Title;
